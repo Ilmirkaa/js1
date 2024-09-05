345dd9324b5c6fabda8f6806ac34e9cf96b7d994
ilmirkaa <galimovailmira06@gmail.com>
2024-09-05 19:52:15 +0300

    first commit

---
+++ assets/main.js
@@ -0,0 +1,10 @@
+"use strict";
+alert("Я JavaScript")
+
+let surname = prompt("введите фамилия", 'nurl');
+let name = prompt("введите имя", 'name');
+let otch = prompt("введите отчество", 'otch');
+alert(`твое ФИО ${surname} ${name} ${otch}`);
+
+let isTrue = confirm("это вы?");
+alert(isTrue);
